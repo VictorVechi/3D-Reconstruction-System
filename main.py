aa8f40709ccec992874123bec4d0118820e45371
LeonardoSalgado04 <53799801+Salgado2004@users.noreply.github.com>
2022-09-19 17:17:28 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,29 @@
 import buscarArquivo as search
 import tratarImgs as img2
 import mapaTextura as texture
-#import manipularPCD as pcd
+import manipularPCD as features
 
 name = input("Insira o nome da pasta (Obs: O nome também será usado para nomear o arquivo):\n")
 imgs, pcd = search.search(name)
 descritores = []
+
+print("Tratando imagens")
 for file in imgs:
     img, f_img, aux_img = img2.getImgs(name, file)
     x, y, w, h, aux_img = img2.getContours(img, aux_img)
     dicto = {'img': file, 'x': x, 'y': y, 'w': w, 'h': h}
     descritores.append(dicto)
-    print(len(descritores), "de", len(imgs))
+print("!!..")
 
 texture.mapping(descritores, name)
+
+print("Manipulando pcd")
+vertices = []
+for file in pcd:
+    pontos = features.getPoints(file, name, descritores)
+    vertices.append(pontos)
+    #Provavelmente tem falhas
+
+print("!!!!")
+
+#pcd.createObj(maiorx, menorx, maiory, menory)
