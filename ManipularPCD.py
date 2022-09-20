773689cea1937da9b80fd2a87188b5464a293dd8
LeonardoSalgado04 <53799801+Salgado2004@users.noreply.github.com>
2022-09-20 17:53:32 -0300

    Update ManipularPCD.py

--- ManipularPCD.py
+++ ManipularPCD.py
@@ -1,13 +1,21 @@
 import numpy as np
 from pyntcloud import PyntCloud as pcd
 
+def filterPoints(index, vertices):
+    if index >= 2:
+        i1 = vertices[index-1]
+        i2 = vertices[index-2]
+        diffX = i1[0] - i2[0]
+        diffY = i1[1] - i2[1]
+        if diffX < 0.0002 or diffY < 0.0002:
+            vertices.pop(index-1)
+
 def filterValues(x1, y1, x2, y2):
     menorx = ((x1/7.391630)/100)+(-0.375560)
     maiorx = ((x2/7.391630)/100)+(-0.375560)
     menory = ((y1/7.391630)/100)+(-0.345560)
     maiory = ((y2/7.391630)/100)+(-0.365560)
     return maiorx, menorx, maiory, menory
-
 
 def getPoints(file, pasta, descritores):
     path = "dataset/"+pasta+"/"+file
@@ -33,23 +41,21 @@ def getPoints(file, pasta, descritores):
         if i[0] > maiorx or i[0] < menorx or i[1] > maiory or i[1] < menory:
             continue
         else:
-            vertices.append(pontos)
-
+            vertices.append(i)
+            #A função de filtrar ainda esta estranha
+            #Ele esta apagando pontos demais
+            #filterPoints(len(vertices), vertices)
+            
     return vertices
 
 
-# def createObj(maiorx, menorx, maiory, menory):
-#    meshfile = open("obj/Yakissoba_1.obj", "w")
-#    meshfile.write("# Reconstrução Leonardo e Victor\n")
-#    meshfile.write("# IFPR - Campus Pinhais\n")
-#    progresso = 0
-#    for i in vertices:
-#        print(f"{progresso} de {len(vertices)}")
-#        if i[0] > maiorx or i[0] < menorx or i[1] > maiory or i[1] < menory:
-#            progresso += 1
-#            continue
-#        else:
-#            progresso += 1
-#            meshfile.write(f"v {i[0]} {i[1]} {i[2]}\n")
-#        
-#    meshfile.close()
+def createObj(vertices, file):
+    file = file.split(".")
+    path = "obj/"+str(file[0])+".obj"
+    meshfile = open(path, "w")
+    meshfile.write("# Reconstrução Leonardo e Victor\n")
+    meshfile.write("# IFPR - Campus Pinhais\n")
+    for i in vertices:
+        meshfile.write(f"v {i[0]} {i[1]} {i[2]}\n")
+        
+    meshfile.close()
