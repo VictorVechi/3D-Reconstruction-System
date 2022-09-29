3d02a95ed02cc690dd1583c84a211b9e796c6a28
LeonardoSalgado04 <53799801+Salgado2004@users.noreply.github.com>
2022-09-29 18:54:33 -0300

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,9 @@ import tratarImgs as img2
 import mapaTextura as texture
 import manipularPCD as features
 
+#Inicia o programa pedindo o nome da pasta, que é buscado nos arquivos com a classe search
 name = input("Insira o nome da pasta (Obs: O nome também será usado para nomear o arquivo):\n")
+#Única função da classe, busca arquivos dentro da pasta com o nome indicado
 imgs, pcd = search.search(name)
 descritores = []
 
@@ -13,15 +15,25 @@ for file in imgs:
     x, y, w, h, aux_img = img2.getContours(img, aux_img)
     dicto = {'img': file, 'x': x, 'y': y, 'w': w, 'h': h}
     descritores.append(dicto)
-print("!!..")
+print("!!...")
 
 texture.mapping(descritores, name)
 
+def reordena(n):
+    idx = n["ang"]	
+    return idx
+
 print("Manipulando pcd")
-mediaGeral = []
+vertices = []
 for file in pcd:
-    pontos, mediaFace = features.getPoints(file, name, descritores)
-    mediaGeral.append(mediaFace)
-    pontos = features.rotatePoints(pontos)
-    features.createObj(pontos, file)
-print("!!!!")
+    pontos, idx = features.getPoints(file, name, descritores)
+    dicto = {"ang": idx, "pontos": pontos}
+    vertices.append(dicto)
+
+vertices.sort(key = reordena)
+
+f_vertices = features.corrigeOrientacao(vertices)
+print("!!!!.")
+
+
+features.createObj(f_vertices, name)
