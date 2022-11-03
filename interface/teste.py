5b0d586cdb2190a2bfd9b2aaf476119448ef0b7f
victor vechi <victorhvechi@gmail.com>
2022-11-03 19:58:49 -0300

    avanço na integração das telas e criação da pasta de diagramas

--- interface/teste.py
+++ interface/teste.py
@@ -1,33 +1,17 @@
 import os
 import shutil
 import sys
+import easygui
 from unittest.main import MAIN_EXAMPLES
 from PyQt6.QtWidgets import QApplication, QWidget, QPushButton, QLabel, QLineEdit, QFileDialog, QMainWindow
-
 app = QApplication(sys.argv)
 
-class JanelaPCD(QMainWindow):
+
+class JanelaPasta(QWidget):
     def __init__(self):
         super().__init__()
+        self.le = QLineEdit(self)
 
-        self.resize(600,500)
-        self.setFixedSize(600,500)
-        self.setWindowTitle("Selecionar Pcd")
-
-        btn = QPushButton("Selecionar arquivo",self)
-        btn.setGeometry(200,200,200,35)
-        btn.setStyleSheet('background-color:#55A38B; color:#FFFFFF')
-       # btn.clicked.connect(self.diretorio)
-
-    #def diretorio(self):
-       # self.file = QFileDialog.getExistingDirectory
-       # self.txt_path.setText(self.file)
-        #self.file = str(self.file)
-
-le = QLineEdit()
-class JanelaPasta(QMainWindow):
-    def __init__(self):
-        super().__init__()
         self.resize(600,500)
         self.setFixedSize(600,500)
         self.setWindowTitle("Criar Pasta para PCDS")
@@ -36,21 +20,25 @@ class JanelaPasta(QMainWindow):
         btn.setGeometry(200,300,200,35)
         btn.setStyleSheet('background-color:#55A38B; color:#FFFFFF')
         btn.clicked.connect(self.criarPasta)
-
+        
         label = QLabel("Insira o nome da pasta:",self)
         label.adjustSize()
         label.setStyleSheet('font-size:15pt arial;')
         label.move(100,200)
 
-        le.setGeometry(310,200,200,30)
+        self.le.setGeometry(250,195,250,30)
+    def criarPasta(self):
+        try:
+            os.mkdir('dataset/'+self.le.text())
+            main.show()
+            self.close()
+            main.selecionarArquivos()
+        except:
+            print("Deu erro")
 
-    def criarPasta():
-        pasta = le.text()
-        print(pasta)
+
 
 class MainWindow(QMainWindow):  
-    janelapcd = JanelaPCD()
-    janelapasta = JanelaPasta()
     def __init__(self):
         super().__init__()
 
@@ -61,24 +49,36 @@ class MainWindow(QMainWindow):
         btn = QPushButton("Criar pasta",self)
         btn.setGeometry(200,200,200,35)
         btn.setStyleSheet('background-color:#55A38B; color:#FFFFFF')
-        btn.clicked.connect(self.btn1)
+        btn.clicked.connect(self.janelaCriarPasta)
 
-        btn2 = QPushButton("Seleção de PCD",self)
+        btn2 = QPushButton("Selecionar arquivos",self)
         btn2.setGeometry(200,300,200,35)
         btn2.setStyleSheet('background-color:#55A38B; color:#FFFFFF')
-        btn2.clicked.connect(self.btn2)
+        btn2.clicked.connect(self.selecionarArquivos)
+        
+        btn3 = QPushButton("Criar 3d",self)
+        btn3.setGeometry(200,300,200,35)
+        btn3.setStyleSheet('background-color:#55A38B; color:#FFFFFF')
+        #btn3.clicked.connect(self.criar3d)
 
-    def btn1(self):
-        main.close()
-        self.janelapasta.show()
-
-    def btn2(self):
-        main.close()
-        self.janelapcd.show()
-
+    def janelaCriarPasta(self):
+        try:
+            main.close()
+            janela.show()
+        except:
+            print("Erro ao abrir janela de criação de pastas")
+    def selecionarArquivos(self):
+        try:
+            for i in range(8):
+                path = easygui.fileopenbox()
+                path2 = easygui.diropenbox()
+                shutil.copy2(path, path2)
+        except:
+            print("me mama")
 def rodar():
     main.show()
     app.exec()        
 
 main = MainWindow()
+janela = JanelaPasta()
 rodar()
